75eb6853243dcf990628372bf7ba73c801ed49fb
Zi <zi.s.gao@gmail.com>
2018-01-04 22:48:10 -0500

    added responsiveness to menu and added more icons

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,20 @@
+function extendMenu(){
+    if ($("#menu_icon").attr("src") == './src/menu_gif_white.gif') {
+        $("#menu_icon").attr("src", "./src/close_icon_black.png");
+        $("#home_icon").attr("src", "./src/logo_black.png");
+        $("#nav_page").css("left", "0%");
+        $("#nav_page").css("opacity", "1");
+        $("#title_text").css("color", "black")
+    } else if ($("#menu_icon").attr("src") == './src/close_gif.gif'){
+        $("#menu_icon").attr("src", "./src/menu_icon_white.png");
+        $("#home_icon").attr("src", "./src/logo_white.png");
+        $("#nav_page").css("left", "-100%");
+        $("#nav_page").css("opacity", "0");
+        $("#title_text").css("color", "rgb(255, 253, 255)");
+    }
+}
 
+
+function resize(){
+    //alert(window.innerWidth);
+}
