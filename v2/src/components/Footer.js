2be2ef2544d60085bbf9724bc247a063fc1f34c8
Zi Gao <zi.s.gao@gmail.com>
2018-12-25 09:39:08 -0500

    finished footer code

--- v2/src/components/Footer.js
+++ v2/src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const Footer = () => (
-    <div>
-    </div>
+    <footer className='Footer width-100'>
+        <div className='Footer_icon_container'>
+            <img id='email_icon' className='Footer_icon' src='./images/email_icon_black.png'/>
+            <img className='Footer_icon' src='./images/linkedin_icon_black.png'/>
+            <img className='Footer_icon' src='./images/github_icon_black.png'/>
+            <img className='Footer_icon' src='./images/instagram_icon_black.png'/>
+            <img className='Footer_icon' src='./images/facebook_icon_black.png'/>
+        </div>
+        <p className='Footer_logo_text'>
+            Made With <i className="em em-coffee"></i> && <i className="em em-blue_heart"></i>
+        </p>
+    </footer>
 );
 
 export default Footer;
