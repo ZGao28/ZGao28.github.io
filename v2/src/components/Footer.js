1420e99b9e8d2e5e5c6955f2c6b07339043309da
Zi Gao <zi.s.gao@gmail.com>
2018-12-25 10:19:22 -0500

    added links

--- v2/src/components/Footer.js
+++ v2/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+const goToLink = (link) => {
+    console.log(link);
+    location.href = link;
+}
+
 const Footer = () => (
     <footer className='Footer width-100'>
         <div className='Footer_icon_container'>
-            <img id='email_icon' className='Footer_icon' src='./images/email_icon_black.png'/>
-            <img className='Footer_icon' src='./images/linkedin_icon_black.png'/>
-            <img className='Footer_icon' src='./images/github_icon_black.png'/>
-            <img className='Footer_icon' src='./images/instagram_icon_black.png'/>
-            <img className='Footer_icon' src='./images/facebook_icon_black.png'/>
+            <img className='Footer_icon' src='./images/email.png' onClick={()=>goToLink('mailto:zi.s.gao@gmail.com?Subject=Hello')}/>
+            <img className='Footer_icon' src='./images/linkedin.png' onClick={()=>goToLink('https://www.linkedin.com/in/zigao')}/>
+            <img className='Footer_icon' src='./images/github.png' onClick={()=>goToLink('https://www.github.com/zgao28')}/>
+            <img className='Footer_icon' src='./images/blog.png' onClick={()=>goToLink('http://zigao.io/blog')}/>
+            <img className='Footer_icon' src='./images/resume.png' onClick={()=>goToLink('./images/zi_gao_resume.pdf')}/>
         </div>
         <p className='Footer_logo_text'>
             Made With <i className="em em-coffee"></i> && <i className="em em-blue_heart"></i>
