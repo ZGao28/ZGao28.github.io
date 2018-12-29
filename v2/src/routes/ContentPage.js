75583d4f4e7c68b2adc4f4b95f14806f96571f0c
Zi Gao <zi.s.gao@gmail.com>
2018-12-28 21:40:31 -0500

    started on main center components

--- v2/src/routes/ContentPage.js
+++ v2/src/routes/ContentPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ContentPage = () => (
-    <div>
+    <div className='center-div ContentPage'>
+        <div>
+            Blog
+        </div>
+        <div>
+            Coding<br></br>Videos
+        </div>
+        <div>
+            Vlogs
+        </div>
     </div>
 );
 
