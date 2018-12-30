5a1d877a9a5b4e4122f84fc9b4b3e8f8b80600d2
Zi Gao <zi.s.gao@gmail.com>
2018-12-29 19:11:45 -0500

    finished about page

--- v2/src/routes/AboutPage.js
+++ v2/src/routes/AboutPage.js
@@ -1,33 +1,16 @@
 import React from 'react';
 
 const AboutPage = () => (
-    <div>
-        <p id="about_paragraph">
-				Hey there
-				<i className="em em-wave"></i> ! My name is Zi, and I am currently a first year Software Engineering student at the University of Ottawa.
-				<br/>
-				<br/> I have a passion for technology and entrepreneurship, and I love to explore unique ideas. By constantly learning new technologies
-				and working on different projects, I aim to solve difficult problems and improve the lives of people around the
-				<i className="em em-earth_americas"></i> . As of now I am interested in backend/infrastructure development, virtual reality, and machine learning, but I also like doing UI and
-				UX design as a hobby!
-				<br/>
-				<br/> If you want to
-				<i className="em em-speech_balloon"></i> a business, or startup idea, or if you have any inquiries about my qualifications and past work,
-				feel free to contact me! I am always willing to meet over a cup of
-				<i className="em em-coffee"></i> !
-				<br/>
-				<br/>
-				<b>My favourite quote:</b>
-				<br/>
-				<br/>
-				<i>"Those who are crazy enough to think that they can change the world are the ones who do."</i>
-				<br/>
-				<br/> - Steve Jobs
-				<br/>
-				<br/>
-				<br/>
-				<span>* This Section is Undergoing a Major Overhaul - Check Back Soon to See the End Result! * </span>
-			</p>
+	<div className='center-div AboutPage'>
+        <p className="AboutPage_paragraph">
+			<h2 className='AboutPage_hey'>
+				to learn and to create - that is my raison d'etre 	
+			</h2>
+			<br/>
+			Hey there <i className='em em-wave'></i> my name is Zi, and I am currently a second year Software Engineering student at the University of Ottawa.  
+			I am an autodidact with a thirst for gaining a deeper understanding of how the world truly works <i className='em em-gear'></i> ;
+			and it is my passion to apply this knowledge that I acquire to solve difficult problems.
+		</p>
     </div>
 );
 
