90ca2bc6438d829b2583c5e2e5828ce1f069ef52
Zi Gao <zi.s.gao@gmail.com>
2018-12-31 09:23:52 -0500

    added project page stuff

--- v2/src/routes/ProjectsPage.js
+++ v2/src/routes/ProjectsPage.js
@@ -1,21 +1,117 @@
 import React from 'react';
 
 const ProjectPage = () => (
-    <div className='ProjectPage center-div'>
+    <div className='ProjectPage center-div-special-2'>
         <div className='Project_row'>
-            <div className='Project_long_div'></div>
-            <div className='Project_short_div'></div>
+            <div className='Project_long_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/social_test2.jpeg)",
+                top: "47%"
+            }}>
+                <h1>lynqup</h1>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/money_test.jpeg)",
+                top: "47%"
+            }}>
+                <h1>price check</h1>
+            </div>
         </div>
         <div className='Project_row'>
-            <div className='Project_short_div'></div>
-            <div className='Project_long_div'></div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/food_test.jpeg)",
+                top: "47%"
+            }}>
+                <h1>foodie</h1>
+            </div>
+            <div className='Project_long_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/fighting_test.jpeg)",
+                top: "47%"
+            }}>
+                <h1>virtual fighterz</h1>
+            </div>
         </div>
         <div className='Project_row'>
-            <div className='Project_short_div'></div>
-            <div className='Project_short_div'></div>
-            <div className='Project_short_div'></div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/parkme.jpeg)",
+                top: "47%"
+            }}>
+                <h1>parkme</h1>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/more_projects.jpeg)",
+                top: "47%"
+            }}>
+                <h2>more projects</h2>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/idea_test.jpeg)",
+                top: "47%"
+            }}>
+                <h2>future projects</h2>
+            </div>
         </div>
     </div>
 );
 
 export default ProjectPage;
+
+/*
+
+import React from 'react';
+
+const ProjectPage = () => (
+    <div className='ProjectPage center-div-special-2'>
+        <div className='Project_row'>
+            <div className='Project_long_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/social_test2.jpeg)",
+                marginLeft: "-50px",
+                marginTop: "-10px"
+            }}>
+                <h1>lynqup</h1>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/money_test.jpeg)",
+                marginTop: "-30px"
+            }}>
+                <h1>price check</h1>
+            </div>
+        </div>
+        <div className='Project_row'>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/food_test.jpeg)",
+                marginLeft: "10px"
+            }}>
+                <h1>foodie</h1>
+            </div>
+            <div className='Project_long_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/fighting_test.jpeg)",
+                marginRight: "-50px"
+            }}>
+                <h1>virtual fighterz</h1>
+            </div>
+        </div>
+        <div className='Project_row'>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/parkme.jpeg)",
+                marginLeft: "-50px",
+                marginTop: "10px"
+            }}>
+                <h1>parkme</h1>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/more_projects.jpeg)",
+                marginTop: "30px"
+            }}>
+                <h2>more projects</h2>
+            </div>
+            <div className='Project_short_div' style={{
+                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(./images/idea_test.jpeg)",
+            }}>
+                <h2>future projects</h2>
+            </div>
+        </div>
+    </div>
+);
+
+export default ProjectPage;
+*/
